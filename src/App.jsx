db2344edd803935077d81d05db461d2e75252c90
Nicky-Pavlov <eaa25npa@students.eaaa.dk>
2026-04-13 18:15:09 +0200

    added pngs in asstes and guy on main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import babyStatue from './assets/baby statue.png'
+import guy from './assets/guy.png'
 
 function App() {
   return (
     <div className="app">
       <div className="grid-container">
-        <div className="box"></div>
+        <div className="box">
+          <img src={guy} alt="Guy" className="box-image" />
+          <h2 className="box-title">Info</h2>
+        </div>
         <div className="box">
           <img src={babyStatue} alt="Baby Statue" className="box-image" />
           <h2 className="box-title">Build a statue</h2>
